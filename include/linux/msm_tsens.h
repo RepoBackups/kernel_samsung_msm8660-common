251f3a3ff614998ebb40bedfb0e12ea20bc87332
Paul Keith <javelinanddart@gmail.com>
2015-06-09 12:26:19 -0500

    tsens8x60: Update driver to upstream
    Signed-off-by: Paul Keith <javelinanddart@gmail.com>

--- include/linux/msm_tsens.h
+++ include/linux/msm_tsens.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2011, The Linux Foundation. All rights reserved.
+ * Copyright (c) 2011-2013, The Linux Foundation. All rights reserved.
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 2 and
@@ -21,7 +21,8 @@
 enum platform_type {
 	MSM_8660 = 0,
 	MSM_8960,
-	MSM_9615,
+	MDM_9615,
+	APQ_8064,
 	MSM_TYPE
 };
 
@@ -41,4 +42,19 @@ struct tsens_device {
 int32_t tsens_get_temp(struct tsens_device *dev, unsigned long *temp);
 int msm_tsens_early_init(struct tsens_platform_data *pdata);
 
+static inline int __init tsens_tm_init_driver(void)
+{ return -ENXIO; }
+static inline int tsens_get_sw_id_mapping(
+				int sensor_num, int *sensor_sw_idx)
+{ return -ENXIO; }
+static inline int tsens_get_hw_id_mapping(
+				int sensor_sw_id, int *sensor_hw_num)
+{ return -ENXIO; }
+#endif
+
+#if defined (CONFIG_THERMAL_TSENS8X60) || (CONFIG_THERMAL_TSENS8960)
+int tsens_get_max_sensor_num(uint32_t *tsens_num_sensors);
+#else
+static inline int tsens_get_max_sensor_num(uint32_t *tsens_num_sensors)
+{ return -ENXIO; }
 #endif /*MSM_TSENS_H */
